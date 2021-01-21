41efa244dbeda2733e0fab8dda1213d0caebdf4e
Mariya <mariya.podosinova@gmail.com>
2021-01-21 17:53:35 +0000

    rafactor Day4 main challenge

--- RockPaperScissorsGame/main.py
+++ RockPaperScissorsGame/main.py
@@ -30,34 +30,33 @@ scissors = '''
 print("Rock, Paper, Scissors Game")
 player = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors \n"))
 
-if player==0:
-  print(rock)
-elif player==1:
-  print(paper)
-elif player==2:
-  print(scissors)
+if player>2 or player<0:
+  print("You typed an invalid number. You lose")
 
-rundom_num = random.randint(0,2)
-computer = [rock, paper, scissors][rundom_num]
-print(f"Computer chose: {rundom_num}\n {computer}")
-
-
-if player==rundom_num:
-  print("Drow")
-elif player==0:  
-  if rundom_num==2:
-    print("You win")
-  else:
-    print("You lose")
-elif player==1:
-  if rundom_num==0:
-    print("You win")
-  else:
-    print("You lose")
-elif player==2:
-  if rundom_num==1:
-    print("You win")
-  else:
-    print("You lose")
 else:
-    print("You typed an invalid number. You lose")
+  game_images=[rock, paper, scissors]
+  print(game_images[player])
+
+
+  rundom_num = random.randint(0,2)
+  computer = game_images[rundom_num]
+  print(f"Computer chose: {rundom_num}\n {computer}")
+
+
+  if player==rundom_num:
+    print("Drow")
+  elif player==0:  
+    if rundom_num==2:
+      print("You win")
+    else:
+      print("You lose")
+  elif player==1:
+    if rundom_num==0:
+      print("You win")
+    else:
+      print("You lose")
+  elif player==2:
+    if rundom_num==1:
+      print("You win")
+    else:
+      print("You lose")
