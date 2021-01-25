4bac1b65966ea6e2f02f777afd7a5074479d054b
Mariya <mariya.podosinova@gmail.com>
2021-01-25 15:56:15 +0000

    add Day8 main challenge Part 1 first attempt

---
+++ CaesarCipher/main.py
@@ -0,0 +1,13 @@
+# First Part
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+def encrypt(text, shift):
+    cipher_text=""
+    for letter in text:
+        idx = alphabet.index(letter)
+        cipher_text+=alphabet[idx+shift]
+   
+encrypt(text, shift)
