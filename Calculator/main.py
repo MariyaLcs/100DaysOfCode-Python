3441bd8e40304c2a3204deca70f053e399442e6a
Mariya <mariya.podosinova@gmail.com>
2021-01-27 11:28:42 +0000

    refactor main challenge part1

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,14 +1,34 @@
+#Part1
+#https://repl.it/@MashaPodosinova/calculator-start#main.py
 from art import logo
 print(logo)
 
-first_number = (input("What is the first number? "))
-operation = input("+   -   *   /   \nPick an operation ")
-next_number = (input("What is the next number? "))
+# add
+def add(n1, n2):
+    return n1 + n2
 
-calculation = True
+# subtract
+def subtract(n1, n2):
+    return n1 - n2
 
-def math(first_number, operation, next_number):
-    result = eval(first_number + operation + next_number)
-    print(f"{first_number}  {operation}  {next_number} = {result}")
-    return result
-math(first_number, operation, next_number)
+# multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {"+" : add, "-" : subtract, "*" : multiply, "/" : divide}
+
+num1 = int(input("What is the first number? "))
+num2 = int(input("What is the next number? "))
+
+for operation in operations:
+    print(operation)
+
+operation_symbol = input("Pick an operation from the line above ")
+# result = eval(num1 + operation + next_number)
+result = operations[operation_symbol](num1, num2)
+
+print(f"{num1}  {operation_symbol}  {num2} = {result}")
