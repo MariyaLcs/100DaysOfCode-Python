b4be8959014d3e9042a2b21a2e0159767626b95b
Mariya <mariya.podosinova@gmail.com>
2021-01-27 13:25:07 +0000

    add final refactor for thr main challenge

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,34 +1,46 @@
-#Part1
 #https://repl.it/@MashaPodosinova/calculator-start#main.py
+from replit import clear
 from art import logo
-print(logo)
 
-# add
 def add(n1, n2):
-    return n1 + n2
+  return n1 + n2
 
-# subtract
 def subtract(n1, n2):
-    return n1 - n2
+  return n1 - n2
 
-# multiply
 def multiply(n1, n2):
-    return n1 * n2
+  return n1 * n2
 
-# divide
 def divide(n1, n2):
-    return n1 / n2
+  return n1 / n2
 
-operations = {"+" : add, "-" : subtract, "*" : multiply, "/" : divide}
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
 
-num1 = int(input("What is the first number? "))
-num2 = int(input("What is the next number? "))
+def calculator():
+  print(logo)
 
-for operation in operations:
-    print(operation)
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-operation_symbol = input("Pick an operation from the line above ")
-# result = eval(num1 + operation + next_number)
-result = operations[operation_symbol](num1, num2)
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
 
-print(f"{num1}  {operation_symbol}  {num2} = {result}")
+calculator()
