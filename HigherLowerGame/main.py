2b10f8c8ecca0d58aaaa8e28317f06c4990db1c3
Mariya <mariya.podosinova@gmail.com>
2021-01-31 19:26:10 +0000

    refactor main challenge for the day 14, right answer always option A

--- HigherLowerGame/main.py
+++ HigherLowerGame/main.py
@@ -1,41 +1,45 @@
-#http://www.higherlowergame.com/
-# https://repl.it/@MashaPodosinova/higher-lower-start#main.py
-
 from random import randint
 from game_data import data
-# from replit import clear
-from art import *
+from replit import clear
+from art import logo, vs
 print(logo)
 
 score = 0
 game_on = True
 
-while game_on: 
+def check_answer(player_answer, count_a, count_b):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if count_a > count_b:
+    return player_answer == "A"
+  else:
+    return player_answer == "B"
 
-    option_a = data[randint(0, len(data)-1)]
-    option_b =data[randint(0, len(data)-2)]
+option_a = data[randint(0, len(data)-1)]
+option_b = data[randint(0, len(data)-1)]
+
+while game_on: 
+    option_a = option_b   
+    count_b = data[randint(0, len(data)-1)]
+
+    while option_a == option_b:
+      option_b = data[randint(0, len(data))]
+
     count_a = option_a['follower_count']
     count_b = option_b['follower_count']
 
-    if count_a  > count_b:
-        letter = "A"
-        right_answer = option_a 
-    else:
-        letter = "B"
-        right_answer = option_b
-   
     print(f"\nCompare A: {option_a['name']}, a {option_a['description']}, from {option_a['country']}.")
     print(vs)
-    print(f"Compare A: {option_b['name']}, a {option_b['description']}, from {option_b['country']}.")
+    print(f"Compare B: {option_b['name']}, a {option_b['description']}, from {option_b['country']}.")
 
     player_answer = input("\nWho has more followers? Type 'A' or 'B': ").upper()
 
-    if player_answer == letter:
-        score += 1
-        print(f"\nYou're right!  Current score: {score}.")
-        option_a = right_answer
-        option_b = data[randint(0, len(data)-1)]
-        
+    is_correct = check_answer(player_answer, count_a, count_b)
+    clear()
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
     else:
-        print(f"\nYou're wrong!  Current score: {score}. Game Over!")
-        game_on = False
+      game_on = False
+      print(f"Sorry, that's wrong. Final score: {score}")
