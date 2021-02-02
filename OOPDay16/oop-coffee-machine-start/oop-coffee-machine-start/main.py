0e68024871b847809d7fd2a064e4fe0924cc4844
Mariya <mariya.podosinova@gmail.com>
2021-02-02 13:21:51 +0000

    add main challenge for the Day16, first attempt

--- OOPDay16/oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ OOPDay16/oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -1,3 +1,22 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+turn_off = False
+
+while not turn_off:
+    order_name = input(f"\nWhat would you like? {Menu().get_items()}: ")
+    if order_name == "off":
+        turn_off = True
+    elif order_name == "report":
+        print(CoffeeMaker().report())
+    else:
+        drink = Menu().find_drink(order_name)
+        
+        print(drink.ingredients)
+        if CoffeeMaker().is_resource_sufficient(drink):
+            
+            user_money = MoneyMachine().make_payment(drink.cost)
+            if user_money:
+                CoffeeMaker().make_coffee(drink)
