384f92519e4c1ece48fcedc2e474a19b2192edeb
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-02-04 19:59:31 +0000

    add starting file for the Day18

---
+++ TurtleGrachics/main.py
@@ -0,0 +1,30 @@
+#https://docs.python.org/3/library/turtle.html
+#https://cs111.wellesley.edu/labs/lab01/colors
+
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("DarkOrchid")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
