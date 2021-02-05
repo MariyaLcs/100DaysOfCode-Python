239a17fab13d085388c56e4bbfabf6109fc74d1b
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-02-05 20:03:46 +0000

    add challenge 1,2 for the Day18

--- TurtleGrachics/main.py
+++ TurtleGrachics/main.py
@@ -7,14 +7,18 @@ timmy_the_turtle = Turtle()
 
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("DarkOrchid")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
 
+# Draw a line
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
 
-
-
-
-
+# Draw a Dashed line
+for _ in range(20):
+    timmy_the_turtle.forward(5)
+    timmy_the_turtle.up()
+    timmy_the_turtle.forward(5)
+    timmy_the_turtle.down()
 
 
 
