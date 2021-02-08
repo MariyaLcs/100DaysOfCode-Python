0f2588d8ebf30722de6936da5445746697b14898
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-02-08 16:29:18 +0000

    add random walk, my first attempt

--- TurtleGrachics/main.py
+++ TurtleGrachics/main.py
@@ -1,7 +1,7 @@
 # https://docs.python.org/3/library/turtle.html
 # https://cs111.wellesley.edu/labs/lab01/colors
 
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, forward, up, down
 
 timmy_the_turtle = Turtle()
 
@@ -22,12 +22,31 @@ timmy_the_turtle.color("DarkOrchid")
 
 # Drawing Different Shapes
 import random
+#
+# for i in range(3, 11):
+#     timmy_the_turtle.color(random.random(), random.random(), random.random())
+#     for _ in range(i):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(360/i)
 
-for i in range(3, 11):
+#Generate a random walk
+
+for _ in range(100):
+    timmy_the_turtle.speed(6)
     timmy_the_turtle.color(random.random(), random.random(), random.random())
-    for _ in range(i):
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.right(360 / i)
+    length = random.randint(0, 5) * 10
+    random_generate = random.randint(1, 4)
+    if random_generate == 1:
+        timmy_the_turtle.forward(length)
+    elif random_generate == 2:
+        timmy_the_turtle.right(90)
+        timmy_the_turtle.forward(length)
+    elif random_generate == 3:
+        timmy_the_turtle.right(-90)
+        timmy_the_turtle.forward(length)
+    else:
+        timmy_the_turtle.backward(length)
+
 
 screen = Screen()
 screen.exitonclick()
