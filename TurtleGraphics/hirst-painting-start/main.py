7d161ac2efdf967dc1b882a3dd6826c024f1a6eb
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-02-09 09:58:56 +0000

    add solution for the main challenge of Day18

--- TurtleGraphics/hirst-painting-start/main.py
+++ TurtleGraphics/hirst-painting-start/main.py
@@ -10,5 +10,33 @@
 #     b = color.rgb.b
 #     new_color = (r, g, b)
 #     rgb_colors.append(new_color)
+import turtle as turtle_module
+import random
+
+turtle_module.colormode(255)
+t = turtle_module.Turtle()
+t.speed(10)
+t.hideturtle()
+
+t.penup()
+t.setheading(225)
+t.forward(300)
+t.setheading(0)
+
+number_of_dots = 100
 
 color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+
+for dot_count in range(1, number_of_dots + 1):
+    t.dot(20, random.choice(color_list))
+    t.forward(50)
+    if dot_count % 10 == 0:
+        t.setheading(90)
+        t.forward(50)
+        t.setheading(180)
+        t.forward(500)
+        t.setheading(0)
+
+screen = turtle_module.Screen()
+screen.exitonclick()
