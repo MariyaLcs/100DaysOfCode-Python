51fc8cfd9e38a9b8050882554f7f43b552c78cce
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-02-11 12:45:25 +0000

    create a Snake Body

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -4,12 +4,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 
-t = Turtle()
-t.shape("square")
-t.color("white")
-
+for s_index in starting_positions:
+    s = Turtle(shape="turtle")
+    s.color("white")
+    s.penup()
+    s.goto(s_index)
 
 
 screen.exitonclick()
