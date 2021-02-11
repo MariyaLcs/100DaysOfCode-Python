6efd1930b319eac83fd5c2b8f943d78ef3369427
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-02-11 16:44:51 +0000

    add control the Snake with a Keypress

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -1,17 +1,21 @@
 from turtle import Turtle
 
-STARTING_POSITION = [(0, 0), (-10, 0), (-20, 0)]
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for s_index in STARTING_POSITION:
-            s = Turtle(shape="turtle")
+            s = Turtle(shape="square")
             s.color("white")
             s.penup()
             s.goto(s_index)
@@ -22,4 +26,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
