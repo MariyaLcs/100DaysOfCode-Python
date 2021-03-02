55f47ae202880b99973a02367c93588c3299dbf9
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-03-02 14:47:51 +0000

    add Score Keeping

--- PongGame/main.py
+++ PongGame/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -35,8 +37,10 @@ while game_is_on:
     # Detect when R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
     # Detect when R paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
