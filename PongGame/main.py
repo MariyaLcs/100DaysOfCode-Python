efdde7c4a1b978f044c674bb32acdcb8c500ee1a
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-03-02 11:44:54 +0000

    write the Paddle Class and Create the Second Paddle

--- PongGame/main.py
+++ PongGame/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -6,27 +7,15 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.turtlesize(5, 1)
-paddle.penup()
-paddle.setposition(350, 0)
-
-
-def up():
-    new_y = paddle.ycor()+20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def down():
-    new_y = paddle.ycor()-20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
 screen.listen()
-screen.onkey(up, "Up")
-screen.onkey(down, "Down")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
