bf365b60c395b9aff8d125090b4a7ec8ed172b99
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-03-02 14:35:07 +0000

    add detection when the Ball goes Out of Bound

--- PongGame/main.py
+++ PongGame/main.py
@@ -32,5 +32,11 @@ while game_is_on:
     # Detect the collision with both paddles
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+    # Detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+    # Detect when R paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
