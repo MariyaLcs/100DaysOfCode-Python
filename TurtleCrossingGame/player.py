50ca8a201e9769d5ca22e9a83c68a0338d6db900
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-03-10 12:23:13 +0000

    add detection when the Player has reached the other side

--- TurtleCrossingGame/player.py
+++ TurtleCrossingGame/player.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
@@ -11,7 +12,7 @@ class Player(Turtle):
         self.color("black")
         self.penup()
         self.left(90)
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
 
     def move_up(self):
         new_y = self.ycor() + MOVE_DISTANCE
@@ -20,3 +21,12 @@ class Player(Turtle):
     def move_down(self):
         new_y = self.ycor() - MOVE_DISTANCE
         self.goto(self.xcor(), new_y)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
