1a46c2836f56c0b77084ed33dc95341b4e2795c5
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-03-25 13:15:17 +0000

    add parat of the course for The Great Squirrel Census Data Analysis(with Pandas)

--- WorkingWithCSV_Day25/squirrel_count/main.py
+++ WorkingWithCSV_Day25/squirrel_count/main.py
@@ -1,1 +1,18 @@
 import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrel = data[data["Primary Fur Color"] == "Gray"]
+red_squirrel = data[data["Primary Fur Color"] == "Cinnamon"]
+black_squirrel = data[data["Primary Fur Color"] == "Black"]
+gray_squirrel_count = len(gray_squirrel)
+red_squirrel_count = len(red_squirrel)
+black_squirrel_count = len(black_squirrel)
+print(gray_squirrel_count, red_squirrel_count, black_squirrel_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Red", "Black"],
+    "Count": [gray_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
