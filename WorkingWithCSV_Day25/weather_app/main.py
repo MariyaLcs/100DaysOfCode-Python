e9506722de1ba354b693e3cfb318b4b43ab561c9
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-03-25 12:50:41 +0000

    add theory part of the course for DataFrames & Series: working with Rows & Columns

--- WorkingWithCSV_Day25/weather_app/main.py
+++ WorkingWithCSV_Day25/weather_app/main.py
@@ -1,17 +1,39 @@
-# with open("weather_data.csv") as data_file:
-#     data = data_file.readlines()
-#     print(data)
-
-# import csv
-#
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-
 import pandas
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
+
+#data_dict = data.to_dict()
+# print(data_dict)
+
+#temp_list = data["temp"].to_list()
+#print(temp_list)
+
+# average_temp = sum(temp_list)/len(temp_list)
+# print(average_temp)
+
+#***Same
+
+#print(data["temp"].mean())
+#print(data["temp"].max())
+
+#***Get Data in Columns
+#print(data["condition"])
+#print(data.condition)
+
+#***Get Data in Row
+#print(data[data.day == "Monday"])
+
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp_F = int(monday.temp)*9/5+32
+# print(monday_temp_F)
+
+#***Create a Dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("new_data.csv")
